b230d124538129d037c893f42142a8ff40e5ad0a
jpatterson-cYRYJA <jpatterson@instructors.2u.com>
2023-07-12 23:13:42 -0700

    finalized index.test.js

--- src/index.test.js
+++ src/index.test.js
@@ -2,18 +2,9 @@ import { cleanup, render } from '@testing-library/react';
 import React from 'react';
 import App from './App';
 
-// set up empty container for test
-let container = null;
-
-beforeEach(() => {
-    container = document.createElement('div');
-    document.body.appendChild(container);
-});
-
 // cleanup after each test
 afterEach(() => {
     cleanup();
-    container.remove();
 })
 test('renders App component within StrictMode', () => {
     render(
