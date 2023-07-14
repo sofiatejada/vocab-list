93ca83705fe7e51f40d86fb9fdfd9de644340d52
jpatterson-cYRYJA <jpatterson@instructors.2u.com>
2023-07-13 21:32:21 -0700

    boiler plate test added for json data. Going to add a simple term to ensure it passes

--- src/components/01/terms.test.js
+++ src/components/01/terms.test.js
@@ -1,2 +1,26 @@
 // testing terms json file for 01
 
+import data from './terms.json';
+
+test('JSON data has required properties and types', () => {
+    data.forEach(term => {
+        expect(term).toHaveProperty('term')
+        expect(term.term).toEqual(expect.any(String));
+
+        expect(term).toHaveProperty('formal_definition');
+        expect(term.formal_definition).toEqual(expect.any(String));
+        
+        expect(term).toHaveProperty('simple_definition');
+        expect(term.simple_definition).toEqual(expect.any(String));
+
+        expect(term).toHaveProperty('w3_link');
+        expect(term.w3_link).toEqual(expect.any(String));
+
+        expect(term).toHaveProperty('md_link');
+        expect(term.md_link).toEqual(expect.any(String));
+
+        expect(term).toHaveProperty('how_to_search');
+        expect(term.how_to_search).toEqual(expect.any(String));
+
+    })
+})
