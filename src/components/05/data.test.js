4c7ee90799198ce989a3a5e3e9fdbd84ba9eb273
jpatterson-cYRYJA <jpatterson@instructors.2u.com>
2023-07-18 13:30:38 -0700

    renamed test files. added 05 terms

---
+++ src/components/05/data.test.js
@@ -0,0 +1,27 @@
+// testing data.json for 04
+
+import data from './data.json';
+
+test('JSON data has required properties and types', () => {
+    data.forEach(term => {
+        // ensure we have basic term setup
+        expect(term).toHaveProperty('term')
+        expect(term.term).toEqual(expect.any(String));
+
+        // expect(term).toHaveProperty('formal_definition');
+        // expect(term.formal_definition).toEqual(expect.any(String));
+
+        // expect(term).toHaveProperty('simple_definition');
+        // expect(term.simple_definition).toEqual(expect.any(String));
+
+        // expect(term).toHaveProperty('w3_link');
+        // expect(term.w3_link).toEqual(expect.any(String));
+
+        // expect(term).toHaveProperty('mdn_link');
+        // expect(term.mdn_link).toEqual(expect.any(String));
+
+        // expect(term).toHaveProperty('how_to_search');
+        // expect(term.how_to_search).toEqual(expect.any(String));
+
+    })
+})
